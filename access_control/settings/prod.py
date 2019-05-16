3a9bf87f805dfc818055334c1afa587a074c3c0f
Edwin Gutierrez <mtphmtph@gmail.com>
2019-05-16 14:59:29 -0700

    cambio de db en prod

--- access_control/settings/prod.py
+++ access_control/settings/prod.py
@@ -7,7 +7,7 @@ ALLOWED_HOST = ['192.168.250.218', 'bluesage.tk']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'controldb',
+        'NAME': 'control',
         'USER': 'catalogo',
         'PASSWORD': 'b1u3s4g3',
         'HOST': '127.0.0.1',
