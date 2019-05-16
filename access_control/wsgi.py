7b5360bb912bada4bdf845c0eeb271bbb52e3840
Edwin Gutierrez <mtphmtph@gmail.com>
2019-05-15 21:23:57 -0700

    coreccion de wsgi para prod

--- access_control/wsgi.py
+++ access_control/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.access_control.prod')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'access_control.settings.prod')
 
 application = get_wsgi_application()
